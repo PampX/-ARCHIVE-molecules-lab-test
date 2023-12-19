d3787f4504ce681ae984d08978bc14b244b4f438
Baptiste D <bdury@next-lab.fr>
2023-12-19 15:24:30 +0100

    dqsdqsd

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import '../../utils/css/footer.css';
-import Logo from "../../assets/images/logoColSVG.svg";
-import { Link } from 'react-router-dom';
-import global from '../../assets/global.js'
-import { useGlobalState } from '../../views/GlobalStateContext';
 
 function Footer() {
 
