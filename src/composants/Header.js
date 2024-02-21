c4f5de38bbb87c9c3a009b2cbd33b6386c4ad4b2
TimotheePharmaNature <tbaudequin@pharmanature.fr>
2024-02-21 17:57:56 +0100

    corrections

--- src/composants/Header.js
+++ src/composants/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../utils/css/header.css';
 import phone from '../images/header/phone.png';
 import logo from '../images/header/logo.png';
+import mail from '../images/header/mail.svg';
 import { Link } from 'react-router-dom';
 import imgLiposome from '../images/home/expert-img.png'
 import logoBlanc from '../images/general/Logo-blanc.png'
@@ -17,7 +18,15 @@ export default function Header() {
         <div className='header-div-container'>
             <div className='header-div-phone'>
                 <img className='header-img-phone' src={phone} alt='icone de téléphone' />
-                <p className='header-p-phone'>+33 (0)4 99 78 08 11</p>
+                <a className='telto' href='tel:+33 4 99 78 08 11'>
+                    <p className='header-p-phone'>+33 (0)4 99 78 08 11</p>
+                </a>
+                <div className='header-svg-mail'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280 320-200v-80L480-520 160-720v80l320 200Z"/></svg>
+                </div>
+                <a className='mailto' href='mailto:contact@molecules-lab.com'>
+                    <p className='header-p-phone'>contact@molecules-lab.com</p>
+                </a>
             </div>
             <div className='header-div-menu'>
                 <Link to='/' className='header-p-menu'>HOME</Link>
