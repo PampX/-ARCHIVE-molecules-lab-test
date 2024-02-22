6a8070d7c31f2a38038037fbd8538d3ff419eafa
TimotheePharmaNature <tbaudequin@pharmanature.fr>
2024-02-22 09:49:11 +0100

    pdv soon

--- src/views/PDVsoon.js
+++ src/views/PDVsoon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../utils/css/pdvsoon.css'
+import liposome from '../images/general/liposome-transparent.png'
 
 export default function PDVsoon() {
     const targetDate = new Date("May 10, 2024").getTime(); 
@@ -14,6 +15,10 @@ export default function PDVsoon() {
         let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
+        days = days < 10 ? "0" + days : days;
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
         return {
             days,
             hours,
@@ -32,11 +37,32 @@ export default function PDVsoon() {
 
     return (
         <div className='pdv-container'>
+            <img className='pdv-liposome' src={liposome}/>
             <div className="pdv-bleu">
                 <h2 className='pdv-title'>LA LISTE DES POINTS DE VENTE ARRIVE</h2>
             </div>
             <div className="pdv-map">
-                <h2 className='pdv-countdown'>{`${timeLeft.days} jours, ${timeLeft.hours} heures, ${timeLeft.minutes} minutes, ${timeLeft.seconds} secondes`}</h2>
+                <div className='countdown'>
+                <div className='countdown-part'>
+                    <h2 className='pdv-countdown-h2'>{timeLeft.days}</h2>
+                    <h3 className='pdv-countdown-h3'>Jours</h3>
+                </div>
+                <h2 className='pdv-countdown-h2'>:</h2>
+                <div className='countdown-part'>
+                    <h2 className='pdv-countdown-h2'>{timeLeft.hours}</h2>
+                    <h3 className='pdv-countdown-h3'>Heures</h3>
+                </div>
+                <h2 className='pdv-countdown-h2'>:</h2>
+                <div className='countdown-part'>
+                    <h2 className='pdv-countdown-h2'>{timeLeft.minutes}</h2>
+                    <h3 className='pdv-countdown-h3'>Minutes</h3>
+                </div>
+                <h2 className='pdv-countdown-h2'>:</h2>
+                <div className='countdown-part'>
+                    <h2 className='pdv-countdown-h2'>{timeLeft.seconds}</h2>
+                    <h3 className='pdv-countdown-h3'>Secondes</h3>
+                </div>
+                </div>
             </div>
         </div>
     );
